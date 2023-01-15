95ad4c1d33d35ae7509791331d3c2402facdd732
lrocha09 <lucas_rocha_5@hotmail.com>
2023-01-15 03:18:18 -0300

    feat: config service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { WebSocketModule } from '../web-socket/web-socket.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WebSocketModule],
+  imports: [WebSocketModule, ConfigModule.forRoot()],
   controllers: [AppController],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.SERVER_PORT);
+
+  const configService = app.get(ConfigService) as ConfigService<any>;
+  const serverPort = configService.get('SERVER_PORT') as number;
+
+  if (!serverPort) throw new Error('A porta da aplicação não foi configurada.');
+  await app.listen(serverPort);
 }
 bootstrap();
